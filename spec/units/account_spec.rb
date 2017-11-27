30158c6422887a0f0a6bd5b4d72b293088284cc2
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-11-27 13:35:51 +0000

    Added account_spec and init balance test. Passed test.

--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -1,4 +1,9 @@
-require '../../lib/account.rb'
+require_relative '../../lib/account.rb'
 
 describe Account do
+  context '#balance' do
+    it 'should initialize with a 0 balance' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
