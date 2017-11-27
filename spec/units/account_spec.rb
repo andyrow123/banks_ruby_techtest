d80586103b009490ce32e1f0ae7f03a696e2ee2d
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-11-27 14:35:56 +0000

    Tested Account for an empty transactions array and added code to fix tests.

--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../../lib/account.rb'
 
 describe Account do
+
+  it 'should initialize with a empty transactions array' do
+    expect(subject.transactions).to be_empty
+  end
+
   context '#balance' do
     it 'should initialize with a 0 balance' do
       expect(subject.balance).to eq 0
