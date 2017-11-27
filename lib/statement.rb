50c5a109bcbb2fb1c7352832ad7760b5d4386c04
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-11-27 21:46:57 +0000

    Added Statement initializes with end_date tests. Passed tests.

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,10 +1,11 @@
 require 'date'
 
 class Statement
-  attr_reader :transactions, :start_date
+  attr_reader :transactions, :start_date, :end_date
 
-  def initialize(transactions, start_date)
+  def initialize(transactions, start_date, end_date)
     @transactions = transactions
     @start_date = Date.parse(start_date)
+    @end_date = Date.parse(end_date)
   end
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -4,7 +4,7 @@ describe Statement do
   let(:transactions) { double :transactions }
   context '#initialize' do
     before(:each) {
-      @statement = Statement.new(transactions, '27/11/2017')
+      @statement = Statement.new(transactions, '27/11/2017', '27/12/2017')
     }
 
     it 'should initialize with list of transactions' do
@@ -13,6 +13,10 @@ describe Statement do
 
     it 'should initialize with a start date' do
       expect(@statement.start_date).to eq Date.parse('27/11/2017')
+    end
+
+    it 'should initialize with a end date' do
+      expect(@statement.end_date).to eq Date.parse('27/12/2017')
     end
   end
 end
